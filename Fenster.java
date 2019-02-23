3ca052f35d2a31c76f36f0793b3cea5778ef5ec8
Durgorn <slotter12@gmail.com>
2019-02-23 23:36:42 +0100

    Add files via upload

--- Fenster.java
+++ Fenster.java
@@ -1,56 +1,92 @@
-import java.awt.FlowLayout;
 import java.awt.Font;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JTextField;
-
+import javax.swing.filechooser.FileNameExtensionFilter;
 
 public class Fenster extends JFrame{
 	JLabel warehouseTxt;
 	JLabel orderTxt;
+	JLabel stateNumTxt;
+	JLabel algo_Type;
 	
-	private JTextField warehouse;
-	private JTextField order;
+	JButton start = new JButton("Start");
+	
 	private JTextField stateNum;
-	private JTextField warehouseName;
-	private JTextField orderName;
+	// private JTextField warehouseName;
+	// private JTextField orderName;
 	
+	JFileChooser wareChoose = new JFileChooser();
+	JFileChooser orderChoose = new JFileChooser();
+	
+	FileNameExtensionFilter txtfilter = new FileNameExtensionFilter("TXT Text-Files", "txt");
 	
 	
 	public Fenster() {
 		super("Programing project Methods of AI");
-		setLayout(new FlowLayout());
 		
-		warehouseName = new JTextField(50);
-		orderName = new JTextField(50);
+		// warehouseName = new JTextField(50);
+		// orderName = new JTextField(50);
+		stateNum = new JTextField(50);
 		
-		//warehouseName.setBounds(5, 5, 100, 20);
+		String[] algorithm_choice = { "Hill-Climbing", "First-Choice Hill-Climbing", "Random Restart", "Simulated Annealing"};
 		
-		warehouseTxt = new JLabel("Warehouse files:");
-		//warehouseTxt.setBounds(5, 5, 400, 20);
-
+		final JComboBox<String> dropdown = new JComboBox<String>(algorithm_choice);
+		
+		wareChoose.setFileFilter(txtfilter);
+		orderChoose.setFileFilter(txtfilter);
+		
+		/*int returnVal = wareChoose.showOpenDialog(parent);
+		if (returnVal == JFileChooser.APPROVE_OPTION) {
+			System.out.println("You chose to open this files: " +
+		            wareChoose.getSelectedFile().getName());
+		}
+		
+		returnVal = orderChoose.showOpenDialog(parent);
+		if (returnVal == JFileChooser.APPROVE_OPTION) {
+			System.out.println("You chose to open this files: " +
+		            orderChoose.getSelectedFile().getName());
+		}*/
+		
+		
+		warehouseTxt = new JLabel("Warehouse file:");
 		orderTxt = new JLabel("Order file:");
-		//orderTxt.setBounds(5, 5, 400, 200);	
+		stateNumTxt = new JLabel("Number of States");
+		algo_Type = new JLabel("Algorithm");
 		
 		Font font = new Font("Arial", Font.PLAIN + Font.BOLD, 30);
 		warehouseTxt.setFont(font);
 		orderTxt.setFont(font);
-		
-		add(orderTxt);
+		algo_Type.setFont(font);
+		stateNumTxt.setFont(font);
+				
 		add(warehouseTxt);
-		add(warehouseName);
-		add(orderName);
+		//add(warehouseName);
+		add(wareChoose);
+		add(orderTxt);
+		//add(orderName);
+		add(orderChoose);
+		add(stateNumTxt);
+		add(stateNum);
+		add(algo_Type);
+		add(dropdown);
+		add(start);
+		
+		
+		setLayout(new GridLayout(5,2));
 		
 		Haendler handler = new Haendler();
-		warehouse.addActionListener(handler);
-		order.addActionListener(handler);
 		stateNum.addActionListener(handler);
-		warehouseName.addActionListener(handler);
-		orderName.addActionListener(handler);
+		// warehouseName.addActionListener(handler);
+		// orderName.addActionListener(handler);
 		
 	}
 	
@@ -59,16 +95,12 @@ public class Fenster extends JFrame{
 		public void actionPerformed(ActionEvent event) {
 			String string ="";
 			
-			if(event.getSource()==warehouse)
-				string=String.format("Warehouse: %s", event.getActionCommand());
-			else if(event.getSource()==order)
-				string=String.format("Order: %s", event.getActionCommand());
-			else if(event.getSource()==stateNum)
+			if(event.getSource()==stateNum)
 				string=String.format("Number of States: %s", event.getActionCommand());
-			else if(event.getSource()==warehouseName)
+			/*else if(event.getSource()==warehouseName)
 				string=String.format("warehouseName: %s", event.getActionCommand());
 			else if(event.getSource()==orderName)
-				string=String.format("orderName: %s", event.getActionCommand());
+				string=String.format("orderName: %s", event.getActionCommand()); */
 			
 			JOptionPane.showMessageDialog(null, string);
 		}

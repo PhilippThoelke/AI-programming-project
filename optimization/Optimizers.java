a803b0149b37eea6371cdad88ac09af457d33cad
PhilippThoelke <p.thoelke@gmx.de>
2019-02-26 20:43:46 +0100

    Add first choice hill climbing for testing

--- optimization/Optimizers.java
+++ optimization/Optimizers.java
@@ -25,7 +25,30 @@ public class Optimizers {
 				}
 			}
 		}
+		return current;
+	}
 
+	public static boolean[] firstChoiceHillClimbing(int psuCount) {
+		boolean[] current = State.randomState(psuCount);
+
+		LinkedList<boolean[]> neighbourhood;
+		boolean[] newState;
+		ListIterator it;
+
+		boolean foundBetter = true;
+		while (foundBetter) {
+			foundBetter = false;
+			neighbourhood = State.unvisitedNeighbourhood(current);
+			it = neighbourhood.listIterator();
+			while (it.hasNext()) {
+				newState = (boolean[]) it.next();
+				if (Loss.loss(current) < Loss.loss(newState)) {
+					current = newState;
+					foundBetter = true;
+					break;
+				}
+			}
+		}
 		return current;
 	}
 

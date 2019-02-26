57eb45f47da31f544bcf39f9117775b4b79c6b1c
PhilippThoelke <p.thoelke@gmx.de>
2019-02-26 09:41:42 +0100

    Refactor

--- frontend/OpenWindow.java
+++ frontend/OpenWindow.java
@@ -2,15 +2,13 @@ package frontend;
 
 import javax.swing.JFrame;
 
-public class Open_Window {
+public class OpenWindow {
 
 	public static void main(String[] args) {
-
 		Fenster unserFenster = new Fenster();
 		unserFenster.setVisible(true);
 		unserFenster.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		unserFenster.setSize(1600, 800);
-
 	}
 
 }

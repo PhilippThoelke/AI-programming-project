198d492dc45a457ffced592c8be5897d104c8c5b
PhilippThoelke <p.thoelke@gmx.de>
2019-02-26 10:29:55 +0100

    Implement preliminary loss function

--- optimization/Loss.java
+++ optimization/Loss.java
@@ -1,15 +1,39 @@
 package optimization;
 
+import java.util.HashSet;
+
+import frame.Warehouse;
+
 public class Loss {
 
 	public static float loss(boolean[] currentState) {
-		int numberPSUs = 0;
-		for (int i = 0; i < currentState.length; i++) {
-			numberPSUs += currentState[i] ? 1 : 0;
-		}
-		// TODO: calculate loss of the current state
+		return (float) numItemsCovered(currentState) / (float) numItemsCarried(currentState);
+	}
 
-		return numberPSUs;
+	public static int numItemsCarried(boolean[] state) {
+		return Warehouse.numItemsCarried(state);
+	}
+
+	public static int numItemsCovered(boolean[] state) {
+		HashSet<Integer> itemsCarried = Warehouse.maskedItems(state);
+		int[] currentOrder = Warehouse.currentOrder();
+
+		int numItemsCovered = 0;
+
+		for (int itemIndex : currentOrder) {
+			if (itemsCarried.contains(itemIndex)) {
+				numItemsCovered++;
+			}
+		}
+		return numItemsCovered;
+	}
+
+	public static int numPSUsUsed(boolean[] state) {
+		int numPSUsUsed = 0;
+		for (int i = 0; i < state.length; i++) {
+			numPSUsUsed += state[i] ? 1 : 0;
+		}
+		return numPSUsUsed;
 	}
 
 }
